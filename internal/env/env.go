1f741d096f1009d7911e5a9433751ff53ec2ac5c
Jess Monter <jessmonter.g@gmail.com>
2025-08-31 10:43:08 -0600

    Add env package, env support (#3)

---
+++ internal/env/env.go
@@ -0,0 +1,28 @@
+package env
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func GetString(key, fallback string) string {
+	val, ok := os.LookupEnv(key)
+	fmt.Printf("Key: %s, Value: %s, Found: %t\n", key, val, ok)
+	if !ok {
+		return fallback
+	}
+	return val
+}
+
+func GetInt(key string, fallback int) int {
+	val, ok := os.LookupEnv(key)
+	if !ok {
+		return fallback
+	}
+	intVal, err := strconv.Atoi(val)
+	if err != nil {
+		return fallback
+	}
+	return intVal
+}
