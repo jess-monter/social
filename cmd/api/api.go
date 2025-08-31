983c8a8fc4020171c0c78db421208758ee4b5777
jess-monter <jessmonter.g@gmail.com>
2025-08-31 08:09:52 -0600

    Add chi to handle routing, add middlewares from chi, add gitignore

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
@@ -14,17 +17,35 @@ type config struct {
 	addr string
 }
 
-func (app *application) mount() *http.ServeMux {
-	mux := http.NewServeMux()
+// We can set the return as http.Handler because chi.NewRouter() returns a value of this type.
+// chi.Router also implements the http.Handler interface.
+// This allows us to change the router implementation without changing the rest of the code.
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
 
-	// Here you would typically register your routes, e.g.:
-	// mux.HandleFunc("/endpoint", app.endpointHandler)
-	mux.HandleFunc("GET /v1/health", app.healthCheckHandler)
+	// This middleware will log the start and end of each request,
+	// along with some useful data about what was requested and how long it took.
+	// It will also recover from any panics and return a 500 error code.
+	// This is a good set of middleware to include in all applications.
+	// You can add more middleware to this stack as needed.
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 
-	return mux
+	// Here we define a route group for version 1 of our API.
+	// This is a good practice to allow for future versions of the API.
+	// All routes for version 1 will be prefixed with /v1.
+	// For example, /v1/health, /v1/endpoint, etc.
+	// This allows us to maintain backward compatibility when we introduce new versions of the API.
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/health", app.healthCheckHandler)
+	})
+
+	return r
 }
 
-func (app *application) run(mux *http.ServeMux) error {
+func (app *application) run(mux http.Handler) error {
 
 	server := &http.Server{
 		Addr:         app.config.addr,
