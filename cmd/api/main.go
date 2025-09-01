ccc97a574ef5ed046a2cf16070e2a117f014ef5c
jess-monter <jessmonter.g@gmail.com>
2025-09-01 07:17:42 -0600

    Setup db connection, add to api entrypoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/jess-monter/social/internal/db"
 	"github.com/jess-monter/social/internal/env"
 	"github.com/jess-monter/social/internal/store"
 )
@@ -11,9 +12,26 @@ func main() {
 
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			addr:         env.GetString("DB_ADDR", "postgres://postgres:postgres@localhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 25),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 25),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewStorage(nil)
+	db, err := db.New(
+		cfg.db.addr,
+		cfg.db.maxOpenConns,
+		cfg.db.maxIdleConns,
+		cfg.db.maxIdleTime,
+	)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.Close()
+
+	store := store.NewStorage(db)
 
 	app := &application{
 		config: cfg,
