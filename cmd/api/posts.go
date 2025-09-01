d502bc6d6f56cb19f2d98e05bdff78c911742d47
jess-monter <jessmonter.g@gmail.com>
2025-09-01 14:45:52 -0600

    Modularize api errors

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"net/http"
 	"strconv"
 
@@ -17,7 +18,7 @@ type CreatePostPayload struct {
 func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
 	var payload CreatePostPayload
 	if err := readJSON(w, r, &payload); err != nil {
-		writeJSONError(w, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 	ctx := r.Context()
@@ -30,12 +31,12 @@ func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request
 	}
 
 	if err := app.store.Posts.Create(ctx, post); err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
 		return
 	}
 
 	if err := writeJSON(w, http.StatusCreated, post); err != nil {
-		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		app.internalServerError(w, r, err)
 		return
 	}
 }
@@ -52,17 +53,19 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 	postIDParam := chi.URLParam(r, "postID")
 	postID, err := parseID(postIDParam)
 	if err != nil {
-		writeJSONError(w, http.StatusBadRequest, "invalid post ID")
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
 	ctx := r.Context()
 	post, err := app.store.Posts.GetPostByID(ctx, postID)
 	if err != nil {
-		if err == store.ErrRecordNotFound {
-			writeJSONError(w, http.StatusNotFound, "post not found")
-		} else {
-			writeJSONError(w, http.StatusInternalServerError, err.Error())
+		switch {
+		case errors.Is(err, store.ErrRecordNotFound):
+			app.notFoundResponse(w, r, err)
+			return
+		default:
+			app.internalServerError(w, r, err)
 		}
 		return
 	}
