b961eb0e65b475350d96bfc6a6a8ac587a06505f
Jess Monter <jessmonter.g@gmail.com>
2025-09-02 08:46:26 -0600

    Add comments models, migrations and enrich posts get to include comments (#11)

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -76,6 +76,13 @@ func (app *application) getPostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	comments, err := app.store.Comments.GetCommentsByPostID(ctx, post.ID)
+	if err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+	post.Comments = comments
+
 	if err := writeJSON(w, http.StatusOK, post); err != nil {
 		writeJSONError(w, http.StatusInternalServerError, err.Error())
 		return
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -9,13 +9,14 @@ import (
 )
 
 type Post struct {
-	ID        int64    `json:"id"`
-	Content   string   `json:"content"`
-	Title     string   `json:"title"`
-	UserID    int64    `json:"user_id"`
-	Tags      []string `json:"tags"`
-	CreatedAt string   `json:"created_at"`
-	UpdatedAt string   `json:"updated_at"`
+	ID        int64      `json:"id"`
+	Content   string     `json:"content"`
+	Title     string     `json:"title"`
+	UserID    int64      `json:"user_id"`
+	Tags      []string   `json:"tags"`
+	CreatedAt string     `json:"created_at"`
+	UpdatedAt string     `json:"updated_at"`
+	Comments  []*Comment `json:"comments"`
 }
 
 type PostStore struct {
@@ -67,5 +68,6 @@ func (s *PostStore) GetPostByID(ctx context.Context, id int64) (*Post, error) {
 			return nil, err
 		}
 	}
+
 	return post, nil
 }
