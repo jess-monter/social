5d8879478b00ed6e13a44ba73496892657f36d98
jess-monter <jessmonter.g@gmail.com>
2025-10-12 18:09:20 -0600

    Add followers model, follow unfollow endpoints

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,35 +1,97 @@
 package main
 
 import (
-	"errors"
+	"context"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/jess-monter/social/internal/store"
 )
 
+type userKey string
+
+const userCtx userKey = "user"
+
 func (app *application) getUserHandler(w http.ResponseWriter, r *http.Request) {
-	userIDParam := chi.URLParam(r, "userID")
-	userID, err := parseID(userIDParam)
-	if err != nil {
+	user := getUserFromContext(r)
+
+	if err := jsonResponse(w, http.StatusOK, user); err != nil {
+		app.internalServerError(w, r, err)
+	}
+}
+
+type FollowUser struct {
+	UserID int64 `json:"user_id"`
+}
+
+func (app *application) followUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := getUserFromContext(r)
+
+	var payload FollowUser
+	if err := readJSON(w, r, &payload); err != nil {
 		app.badRequestResponse(w, r, err)
 		return
 	}
 
-	user, err := app.store.Users.GetUserByID(r.Context(), userID)
-	if err != nil {
-		switch {
-		case errors.Is(err, store.ErrRecordNotFound):
-			app.notFoundResponse(w, r, err)
-			return
-		default:
-			app.internalServerError(w, r, err)
-		}
-		return
-	}
-
-	if err := jsonResponse(w, http.StatusOK, user); err != nil {
+	if err := app.store.Followers.Follow(r.Context(), user.ID, payload.UserID); err != nil {
 		app.internalServerError(w, r, err)
 		return
 	}
+
+	if err := jsonResponse(w, http.StatusNoContent, user); err != nil {
+		app.internalServerError(w, r, err)
+	}
+
+}
+
+func (app *application) unfollowUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := getUserFromContext(r)
+
+	var payload FollowUser
+	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := app.store.Followers.Unfollow(r.Context(), user.ID, payload.UserID); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	if err := jsonResponse(w, http.StatusNoContent, user); err != nil {
+		app.internalServerError(w, r, err)
+	}
+}
+
+func (app *application) userContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		userID, err := parseID(chi.URLParam(r, "userID"))
+		if err != nil {
+			app.badRequestResponse(w, r, err)
+			return
+		}
+
+		ctx := r.Context()
+		user, err := app.store.Users.GetUserByID(ctx, userID)
+
+		if err != nil {
+			switch err {
+			case store.ErrRecordNotFound:
+				app.notFoundResponse(w, r, err)
+				return
+			default:
+				app.internalServerError(w, r, err)
+				return
+			}
+		}
+
+		ctx = context.WithValue(ctx, userCtx, user)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func getUserFromContext(r *http.Request) *store.User {
+	user, _ := r.Context().Value(userCtx).(*store.User)
+	return user
 }
--- internal/store/storage.go
+++ internal/store/storage.go
@@ -23,12 +23,17 @@ type Storage struct {
 		GetCommentsByPostID(context.Context, int64) ([]*Comment, error)
 		Create(context.Context, *Comment) error
 	}
+	Followers interface {
+		Follow(context.Context, int64, int64) error
+		Unfollow(context.Context, int64, int64) error
+	}
 }
 
 func NewStorage(db *sql.DB) Storage {
 	return Storage{
-		Posts:    &PostStore{db},
-		Users:    &UserStore{db},
-		Comments: &CommentStore{db},
+		Posts:     &PostStore{db},
+		Users:     &UserStore{db},
+		Comments:  &CommentStore{db},
+		Followers: &FollowerStore{db},
 	}
 }
