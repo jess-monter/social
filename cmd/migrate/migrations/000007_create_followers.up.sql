1a6cd75ec0696e6a6c73abddaefcc22365938cf1
jess-monter <jessmonter.g@gmail.com>
2025-10-12 18:34:28 -0600

    Add indexes to comments, posts and users

--- cmd/migrate/migrations/000007_create_followers.up.sql
+++ cmd/migrate/migrations/000007_create_followers.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE followers (
-    user_id BIGINT NOT NULL, 
-    follower_id BIGINT NOT NULL,
-    created_at TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
+    user_id BIGSERIAL REFERENCES users(id) ON DELETE CASCADE,
+    follower_id BIGSERIAL REFERENCES users(id) ON DELETE CASCADE,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     PRIMARY KEY (user_id, follower_id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE
