d1350680e21d8b0e888a89fcca087bb3566f7bac
jess-monter <jessmonter.g@gmail.com>
2025-10-17 16:47:54 -0600

    Add search, tags to filters

--- internal/store/pagination.go
+++ internal/store/pagination.go
@@ -3,12 +3,15 @@ package store
 import (
 	"net/http"
 	"strconv"
+	"strings"
 )
 
 type PaginatedFeedQuery struct {
-	Limit  int    `json:"limit" validate:"omitempty,min=1,max=20"`
-	Offset int    `json:"offset" validate:"omitempty,min=0"`
-	Sort   string `json:"sort" validate:"omitempty,oneof=asc desc"`
+	Limit  int      `json:"limit" validate:"omitempty,min=1,max=20"`
+	Offset int      `json:"offset" validate:"omitempty,min=0"`
+	Sort   string   `json:"sort" validate:"omitempty,oneof=asc desc"`
+	Tags   []string `json:"tags" validate:"omitempty,max=5"`
+	Search string   `json:"search" validate:"omitempty,max=100"`
 }
 
 func (fq PaginatedFeedQuery) Parse(r *http.Request) (PaginatedFeedQuery, error) {
@@ -37,6 +40,15 @@ func (fq PaginatedFeedQuery) Parse(r *http.Request) (PaginatedFeedQuery, error) 
 		fq.Sort = sort
 	}
 
+	tags := qs.Get("tags")
+	if tags != "" {
+		fq.Tags = strings.Split(tags, ",")
+	}
+
+	search := qs.Get("search")
+	if search != "" {
+		fq.Search = search
+	}
 	return fq, nil
 
 }
--- internal/store/posts.go
+++ internal/store/posts.go
@@ -147,6 +147,7 @@ func (s *PostStore) Update(ctx context.Context, post *Post) error {
 }
 
 func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedFeedQuery) ([]FeedPost, error) {
+	// TODO: Review f.user_id = $1 OR p.user_id = $1 logic
 	query := `
 		SELECT p.id, p.content, p.title, p.user_id, p.tags, p.created_at, p.updated_at, p.version,
 		u.username,
@@ -155,7 +156,10 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedF
 		LEFT JOIN comments c ON p.id = c.post_id
 		LEFT JOIN users u ON p.user_id = u.id
 		JOIN followers f ON p.user_id = f.follower_id OR p.user_id = $1
-		WHERE f.user_id = $1 OR p.user_id = $1
+		WHERE 
+			f.user_id = $1 OR p.user_id = $1 AND
+			(p.title ILIKE '%' || $4 || '%' OR p.content ILIKE '%' || $4 || '%') AND
+			(p.tags @> $5 OR $5 = '{}')
 		GROUP BY p.id, u.username
 		ORDER BY p.created_at ` + fq.Sort + `
 		LIMIT $2 OFFSET $3
@@ -164,7 +168,7 @@ func (s *PostStore) GetUserFeed(ctx context.Context, userID int64, fq PaginatedF
 	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
 	defer cancel()
 
-	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset)
+	rows, err := s.db.QueryContext(ctx, query, userID, fq.Limit, fq.Offset, fq.Search, pq.Array(fq.Tags))
 
 	if err != nil {
 		return nil, err
